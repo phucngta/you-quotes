f79263d4a71e65cf740d21faa2de3d383e44ea24
phucngta <phucngta@gmail.com>
2018-06-05 20:12:25 +0700

    [Update] view

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import FacebookProvider, { ShareButton } from 'react-facebook';
+import PropTypes from 'prop-types';
 
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import './App.css';
-import PropTypes from 'prop-types';
 
 const { Component } = React;
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.encodeQuoteData = this.encodeQuoteData.bind(this);
 		this.getQuote = this.getQuote.bind(this);
 		this.constructTwitterIconHref = this.constructTwitterIconHref.bind(this);
-		
+
 		this.state = {
 			quoteText: 'Quality means doing it right when no one is looking.',
 			quoteAuthor: 'Henry Ford',
 			twitterIconHref: ''
 		};
 	}
-	
+
 	constructTwitterIconHref() {
 		this.setState({
 			twitterIconHref: this.props.twitterIconHref + encodeURIComponent(`${this.state.quoteText} ${this.state.quoteAuthor}`)
@@ -33,32 +33,32 @@ class App extends Component {
 	encodeQuoteData() {
 		return encodeURIComponent(`${this.state.quoteText} ${this.state.quoteAuthor}`);
 	}
-	
+
 	getQuote() {
-	  $.getJSON(this.props.url, ({ quoteText, quoteAuthor }) => {
-			this.setState({ 
-				quoteText, 
+		$.getJSON(this.props.url, ({ quoteText, quoteAuthor }) => {
+			this.setState({
+				quoteText,
 				quoteAuthor,
 			});
 		}, 'jsonp')
 	}
-	
+
 	render() {
 		return (
 			<div id='app'>
-				<h1 className="title">You Quote</h1>			
+				<h1 className="title">You Quotes</h1>
 				<div className='content'>
-					<Quote 
+					<Quote
 						quoteText={this.state.quoteText}
 						quoteAuthor={this.state.quoteAuthor}
 					/>
-					<TwitterIcon 
+					{/* <TwitterIcon
 						twitterIconHref={this.state.twitterIconHref}
 						constructTwitterIconHref={this.constructTwitterIconHref}
-					/>
-                  <FacebookProvider appId="1545891592136985">
-                    <ShareButton href="http://www.facebook.com" />
-                  </FacebookProvider>
+					/> */}
+					<FacebookProvider appId="1545891592136985">
+						<ShareButton className="btn btn-lg quote-button" href="http://www.facebook.com" />
+					</FacebookProvider>
 					<Button getQuote={this.getQuote} />
 				</div>
 			</div>
@@ -67,35 +67,35 @@ class App extends Component {
 
 }
 
-const Quote = ({ 
-	quoteText, 
-	quoteAuthor = 'Unknown', 
+const Quote = ({
+	quoteText,
+	quoteAuthor = 'Unknown',
 }) => (
-	<blockquote>
-		<i className="fa fa-quote-left"></i>
-		<p className="quote-text">{quoteText}</p>
-		<i className="fa fa-quote-right"></i>
-		<footer className="quote-footer"> — <span>{quoteAuthor}</span></footer>
-	</blockquote>
-);
+		<blockquote>
+			<i className="fa fa-quote-left"></i>
+			<p className="quote-text">{quoteText}</p>
+			<i className="fa fa-quote-right"></i>
+			<footer className="quote-footer"> — <span>{quoteAuthor}</span></footer>
+		</blockquote>
+	);
 
-const TwitterIcon = ({ 
+const TwitterIcon = ({
 	twitterIconHref,
 	constructTwitterIconHref
 }) => (
-	<div className="icon">
-		<a 
-			onClick={constructTwitterIconHref}
-			href={twitterIconHref}
-			target='_blank'><i className="fa fa-twitter"></i>
-		</a>
-	</div>
+		<div className="icon">
+			<a
+				onClick={constructTwitterIconHref}
+				href={twitterIconHref}
+				target='_blank'><i className="fa fa-twitter"></i>
+			</a>
+		</div>
+	);
+
+const Button = ({ getQuote }) => (
+	<button className="btn quote-button pull-right" onClick={getQuote}>New Quote!</button>
 );
 
-const Button = ( {getQuote} ) => (
-		<button className="btn quote-next" onClick={getQuote}>New Quote!</button>
-);
-		
 App.defaultProps = {
 	url: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?",
 	twitterIconHref: "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
